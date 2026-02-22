909903a2560f7e9508329a8c95f2058743ad4e4f
portfolio <ganeshmrsn002@gmail.com>
2026-02-22 12:51:25 +0530

    added react-scroll, resume

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@
 
 const Contact = () => {
   return (
-    <section className="bg-slate-50 dark:bg-slate-900 w-full p-5 md:px-25 md:py-10 md:flex justify-evenly gap-10">
+    <section id="contact" className="bg-slate-50 dark:bg-slate-900 w-full p-5 md:px-25 md:py-10 md:flex justify-evenly gap-10">
         <h2 className="text-2xl font-medium text-sky-700 dark:text-sky-300 mb-5">Contact</h2>
 
         <div className="md:w-md lg:w-2xl font-medium">
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,10 @@
-import Photo from "../assets/Ganesh_S.svg"
-import { FaArrowRightLong } from "react-icons/fa6"
+import Photo from "../assets/Ganesh_S.svg";
+import { FaArrowDownLong } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
-import { TbMail  } from "react-icons/tb";
+import { TbMail } from "react-icons/tb";
+import { Link } from "react-scroll";
+import Resume from "../../public/GaneshS_Resume.pdf";
 
 const Home = () => {
   return (
@@ -15,18 +17,22 @@ const Home = () => {
       <h2 className="text-2xl text-slate-800 dark:text-slate-200">Software Developer</h2>
 
       <div className="flex flex-col md:flex-row gap-8">
-        <button className="flex items-center gap-2 bg-blue-700 text-slate-100 font-medium px-5 py-2 rounded-lg cursor-pointer transition hover:scale-105">
-          Resume <FaArrowRightLong />
-        </button>
-        <button className="px-5 py-2 border-2 border-blue-700 text-slate-800 dark:text-slate-100 font-medium rounded-lg cursor-pointer transition hover:scale-105">
-          Contact me
-        </button>
+        <a href={Resume} target="_blank" rel="noopener noreferrer">
+          <button className="flex items-center gap-2 bg-blue-700 text-slate-100 font-medium px-5 py-2 rounded-lg cursor-pointer transition hover:scale-105">
+            Resume <FaArrowDownLong />
+          </button>
+        </a>
+        <Link to="contact" smooth="easeInOutCubic" duration={700}>
+          <button className="px-5 py-2 border-2 border-blue-700 text-slate-800 dark:text-slate-100 font-medium rounded-lg cursor-pointer transition hover:scale-105">
+            Contact me
+          </button>
+        </Link>
       </div>
 
       <div className="flex gap-10 text-sky-100 dark:text-slate-200">
         <a href="https://linkedin.com/in/ganesh-s002" target="_blank"><FaLinkedin className="text-3xl cursor-pointer transition hover:text-blue-700"/></a>
         <a href="https://github.com/ganeshs-002" target="_blank"><FaGithub className="text-3xl cursor-pointer transition hover:text-blue-700"/></a>
-        <a href="mailto:ganeshmrsn002@gmail.com"><TbMail  className="text-3xl cursor-pointer transition hover:text-blue-700"/></a>
+        <a href="mailto:ganeshmrsn002@gmail.com"><TbMail className="text-3xl cursor-pointer transition hover:text-blue-700"/></a>
       </div>
     </section>
   )
